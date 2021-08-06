c1271747f2af476efb26cd55833b4ffef797e260
Leonardo Anjos <las4@cin.ufpe.br>
2021-08-06 18:00:50 -0300

    Capture command line options

---
+++ index.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const commandLineArgs = require('command-line-args');
+
+const defaultDivision = 2;
+const defaultParticipantsFilePath = 'participants.txt';
+
+const options = commandLineArgs([
+    { name: 'division', alias: 'd', type: Number },
+    { name: 'participants', alias: 'p', type: String }
+]);
+
+const division = options.division || defaultDivision;
+const participantsFilePath = options.participants || defaultParticipantsFilePath;
